ca644b0b2d53302d548696b905778e9790be0d7a
Rongjiehuang <935012782@qq.com>
2020-08-09 14:58:08 +0800

    增加了成果图片
    增加了成果图片

--- Step5_GradientsYUV.py
+++ Step5_GradientsYUV.py
@@ -4,18 +4,19 @@ import matplotlib
 import cv2
 
 import numpy as np
-
+from function.Gradient import mysolve
 from function.GetWeights import getweights
 from function.dct import mydct
 from function.RGB2YUV import RGB2YUV
 seed = 1
 torch.manual_seed(seed)
+IMG_SHAPE = 224
 
 if __name__ == '__main__':
     # 所使用的原始图像载入
 
     img = cv2.imread('Pic/3.png')
-    img = cv2.resize(img,(224,224))
+    img = cv2.resize(img,(IMG_SHAPE,IMG_SHAPE))
     img = img / 255
 
     WR, WG, WB = getweights(img)
@@ -26,24 +27,41 @@ if __name__ == '__main__':
 
     # Step5 : IYUV 2 FYUV
     FYUV = mydct(imgYUV)
+    gY, gU, gV = mysolve(FYUV)
 
-    FY = abs(FYUV[:,:,0])
-    FU = abs(FYUV[:,:,1])
-    FV = abs(FYUV[:,:,2])
+    # print("gR",gY)
+    # print("gG",gU)
+    # print("gB",gV)
 
-    Usensitivity = np.array(FU).sum()/(224*224)
-    Vsensitivity = np.array(FV).sum()/(224*224)
-
-    print("Usensitivity",Usensitivity)
-    print("Vsensitivity",Vsensitivity)
+    gYvalue = np.array(gY).sum()/(IMG_SHAPE*IMG_SHAPE)
+    gUvalue = np.array(gU).sum()/(IMG_SHAPE*IMG_SHAPE)
+    gVvalue = np.array(gV).sum()/(IMG_SHAPE*IMG_SHAPE)
 
 
-    rects1 = plt.bar(x=1,height=Usensitivity, width=0.1, color='yellow', label="Usensitivity")
-    rects2 = plt.bar(x=1,height=Vsensitivity, width=0.1, color='green', label="Vsensitivity")
-    rects0 = plt.bar(x=1,height=0.2-Usensitivity-Vsensitivity, width=0.1, color='red', label="Usensitivity")
+    print("gYvalue",gYvalue)
+    print("gUvalue",gUvalue)
+    print("gVvalue",gVvalue)
+
+    #归一化
+    gYvalue_normal = gYvalue/(gYvalue+gUvalue+gVvalue)
+    gUvalue_normal = gUvalue/(gYvalue+gUvalue+gVvalue)
+    gVvalue_normal = gVvalue/(gYvalue+gUvalue+gVvalue)
+
+
+    print("gYvalue_normal",gYvalue_normal)
+    print("gUvalue_normal",gUvalue_normal)
+    print("gVvalue_normal",gVvalue_normal)
+
+
+    plt.bar(x=1,height=gYvalue_normal, width=0.1, color='black', label="Y",bottom=0)
+    plt.bar(x=1,height=gUvalue_normal, width=0.1, color='blue', label="Y",bottom=gYvalue_normal)
+    plt.bar(x=1,height=gVvalue_normal, width=0.1, color='red', label="V",bottom=gYvalue_normal+gVvalue_normal)
+    # plt.xticks([1, 1.5], ["Human", "DNN"])
     plt.ylim(0, 1)
     plt.ylabel("Sensitivity")
     plt.xlabel("GYUV")
     plt.title("Target:Resnet50")
+
     plt.legend()
+    # plt.savefig('Pic/Figure6_ColorSensitivity.jpg')
     plt.show()
