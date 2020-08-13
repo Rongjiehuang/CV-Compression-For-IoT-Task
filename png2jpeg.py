720a286bcd9ff653da6e2ed185f0826f51c57759
Rongjiehuang <935012782@qq.com>
2020-08-13 22:38:05 +0800

    更新了语义分割评价
    更新了语义分割评价

---
+++ png2jpeg.py
@@ -0,0 +1,22 @@
+import os
+from PIL import Image
+
+DirList = [
+    'Pic/city'
+]
+
+for path in DirList:
+    for filename in os.listdir(path):
+        fullName = os.path.join(path, filename)
+        if fullName.endswith('.jpeg'):
+            mainName, ext = os.path.splitext(fullName)
+            # print(fullName, mainName + '.jpeg')
+            im = Image.open(fullName).convert("RGB")
+            im.save(mainName + '.jpeg', quality=80)
+            os.remove(fullName)
+
+file = open("sample.txt")
+for line in file:
+
+
+file.close()
